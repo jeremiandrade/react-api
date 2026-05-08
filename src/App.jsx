f29dd444879a3953d49fe72c19edac321b2a4431
jeremiandrade <jeremiandrade04@gmail.com>
2026-05-08 21:39:35 +0200

    add cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 
 import { useState, useEffect, use } from 'react'
 
-// E’ arrivato il momento di mettere insieme i concetti appresi creiamo una piccola app che ci mostri un elenco di attori o attrici.
-
-// Usate uno di questi due endpoint, a piacimento:
-
 // Lista di Attrici:   https://lanciweb.github.io/demo/api/actresses/
 
 // Lista di Attori:  https://lanciweb.github.io/demo/api/actors/
 
-// MILESTONE 1
-// Al caricamento dell'applicazione, recuperiamo la lista degli attori e delle attrici dalle API e stampiamoli in console.
 
-// MILESTONE 2
-
-// Prepariamo una card per ciascun attore/attrice, mostrandone le seguenti informazioni:
-
-// nome
-// anno nascita
-// nazionalità
-// biografia
-// immagine
-// riconoscimenti
 
 // MILESTONE 3
 
@@ -39,7 +23,6 @@ function App() {
     fetch(actresses_url)
       .then(response => response.json())
       .then(data => {
-
         console.log(data);
         setActresses(data)
       })
@@ -63,25 +46,78 @@ function App() {
 
   return (
     <>
-      {/* list item actresses */}
-      <div className="list-1_actresses">
+      {/* <div className="list-1_actresses">
         <h2>List Actresses</h2>
         <ol>
           {actresses.map(item => (
             <li key={item.id}>{item.name}</li>
           ))}
         </ol>
-      </div>
+      </div> */}
 
       {/* list item actors */}
-      <div className="list_2_actors">
+
+      {/* <div className="list_2_actors">
         <h2>List Actors</h2>
         <ol>
           {actors.map(actor => (
             <li key={actor.id}>{actor.name}</li>
           ))}
         </ol>
+      </div> */}
+
+
+
+      <div>
+        <h3> Card Actresses</h3>
+        <div className="container">
+          <div className="row">
+
+            {actresses.map(item => (
+              <div className="col" key={item.id}>
+                <div className="card">
+                  <img src={item.image} alt="" />
+                  <div className="card-body">
+                    <h4 className="card-title">{item.name}</h4>
+                    <p className="card-text">{item.biography}</p>
+                    <p>Nazionalità:{item.nationality}</p>
+                    <p>Anno Di Nascità:{item.birth_years}</p>
+                    <p>Riconoscimento:  {item.awards} </p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
+
+
+      <div>
+        <h3> Card Actor</h3>
+        <div className="container">
+          <div className="row">
+
+            {actors.map(item => (
+              <div className="col" key={item.id}>
+                <div className="card">
+                  <img src={item.image} alt="" />
+                  <div className="card-body">
+                    <h4 className="card-title">{item.name}</h4>
+                    <p className="card-text">{item.biography}</p>
+                    <p>Nazionalità:{item.nationality}</p>
+                    <p>Anno Di Nascità:{item.birth_years}</p>
+                    <p>Riconoscimento:  {item.awards} </p>
+                  </div>
+                </div>
+              </div>
+
+            ))
+
+            }
+          </div>
+        </div>
+      </div>
+
 
 
 
